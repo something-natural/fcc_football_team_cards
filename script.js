b3ab65919d307aef2fce3a6426f6067278e40413
something-natural <tsglow@naver.com>
2024-09-05 15:37:37 +0900

    feat: done event listener

--- script.js
+++ script.js
@@ -1,189 +1,223 @@
+// team array
+const myFavoriteFootballTeam = {
+  team: "Argentina",
+  sport: "Football",
+  year: 1986,
+  isWorldCupWinner: true,
+  headCoach: {
+    coachName: "Carlos Bilardo",
+    matches: 7,
+  },
+  players: [
+    {
+      name: "Sergio Almirón",
+      position: "forward",
+      number: 1,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Sergio Batista",
+      position: "midfielder",
+      number: 2,
+      isCaptain: false,
+      nickname: null,
+    },
+     {
+      name: "Ricardo Bochini",
+      position: "midfielder",
+      number: 3,
+      isCaptain: false,
+      nickname: "El Bocha",
+    },
+    {
+      name: "Claudio Borghi",
+      position: "midfielder",
+      number: 4,
+      isCaptain: false,
+      nickname: "Bichi",
+    },
+    {
+      name: "José Luis Brown",
+      position: "defender",
+      number: 5,
+      isCaptain: false,
+      nickname: "Tata",
+    },
+    {
+      name: "Daniel Passarella",
+      position: "defender",
+      number: 6,
+      isCaptain: false,
+      nickname: "El Gran Capitán",
+    },
+    {
+      name: "Jorge Burruchaga",
+      position: "forward",
+      number: 7,
+      isCaptain: false,
+      nickname: "Burru",
+    },
+    {
+      name: "Néstor Clausen",
+      position: "defender",
+      number: 8,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "José Luis Cuciuffo",
+      position: "defender",
+      number: 9,
+      isCaptain: false,
+      nickname: "El Cuchu",
+    },
+    {
+      name: "Diego Maradona",
+      position: "midfielder",
+      number: 10,
+      isCaptain: true,
+      nickname: "El Pibe de Oro",
+    },
+    {
+      name: "Jorge Valdano",
+      position: "forward",
+      number: 11,
+      isCaptain: false,
+      nickname: "The Philosopher of Football",
+    },
+    {
+      name: "Héctor Enrique",
+      position: "midfielder",
+      number: 12,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Garré",
+      position: "defender",
+      number: 13,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Ricardo Giusti",
+      position: "midfielder",
+      number: 14,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Luis Islas",
+      position: "goalkeeper",
+      number: 15,
+      isCaptain: false,
+      nickname: "El loco",
+    },
+    {
+      name: "Julio Olarticoechea",
+      position: "defender",
+      number: 16,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Pedro Pasculli",
+      position: "forward",
+      number: 17,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Nery Pumpido",
+      position: "goalkeeper",
+      number: 18,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Oscar Ruggeri",
+      position: "defender",
+      number: 19,
+      isCaptain: false,
+      nickname: "El Cabezón",
+    },
+    {
+      name: "Carlos Tapia",
+      position: "midfielder",
+      number: 20,
+      isCaptain: false,
+      nickname: null,
+    },
+    {
+      name: "Marcelo Trobbiani",
+      position: "midfielder",
+      number: 21,
+      isCaptain: false,
+      nickname: "Calesita",
+    },
+    {
+      name: "Héctor Zelada",
+      position: "goalkeeper",
+      number: 22,
+      isCaptain: false,
+      nickname: null,
+    },
+  ],
+};
+
 // html elements
 const team = document.getElementById("team");
 const sport = document.getElementById("sport");
 const year = document.getElementById("year");
 const coach = document.getElementById("head-coach");
-const select = document.getElementById("players")
-
-
-// team array
-const myFavoriteFootballTeam = {
-    team: "Argentina",
-    sport: "Football",
-    year: 1986,
-    isWorldCupWinner: true,
-    headCoach: {
-      coachName: "Carlos Bilardo",
-      matches: 7,
-    },
-    players: [
-      {
-        name: "Sergio Almirón",
-        position: "forward",
-        number: 1,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Sergio Batista",
-        position: "midfielder",
-        number: 2,
-        isCaptain: false,
-        nickname: null,
-      },
-       {
-        name: "Ricardo Bochini",
-        position: "midfielder",
-        number: 3,
-        isCaptain: false,
-        nickname: "El Bocha",
-      },
-      {
-        name: "Claudio Borghi",
-        position: "midfielder",
-        number: 4,
-        isCaptain: false,
-        nickname: "Bichi",
-      },
-      {
-        name: "José Luis Brown",
-        position: "defender",
-        number: 5,
-        isCaptain: false,
-        nickname: "Tata",
-      },
-      {
-        name: "Daniel Passarella",
-        position: "defender",
-        number: 6,
-        isCaptain: false,
-        nickname: "El Gran Capitán",
-      },
-      {
-        name: "Jorge Burruchaga",
-        position: "forward",
-        number: 7,
-        isCaptain: false,
-        nickname: "Burru",
-      },
-      {
-        name: "Néstor Clausen",
-        position: "defender",
-        number: 8,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "José Luis Cuciuffo",
-        position: "defender",
-        number: 9,
-        isCaptain: false,
-        nickname: "El Cuchu",
-      },
-      {
-        name: "Diego Maradona",
-        position: "midfielder",
-        number: 10,
-        isCaptain: true,
-        nickname: "El Pibe de Oro",
-      },
-      {
-        name: "Jorge Valdano",
-        position: "forward",
-        number: 11,
-        isCaptain: false,
-        nickname: "The Philosopher of Football",
-      },
-      {
-        name: "Héctor Enrique",
-        position: "midfielder",
-        number: 12,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Oscar Garré",
-        position: "defender",
-        number: 13,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Ricardo Giusti",
-        position: "midfielder",
-        number: 14,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Luis Islas",
-        position: "goalkeeper",
-        number: 15,
-        isCaptain: false,
-        nickname: "El loco",
-      },
-      {
-        name: "Julio Olarticoechea",
-        position: "defender",
-        number: 16,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Pedro Pasculli",
-        position: "forward",
-        number: 17,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Nery Pumpido",
-        position: "goalkeeper",
-        number: 18,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Oscar Ruggeri",
-        position: "defender",
-        number: 19,
-        isCaptain: false,
-        nickname: "El Cabezón",
-      },
-      {
-        name: "Carlos Tapia",
-        position: "midfielder",
-        number: 20,
-        isCaptain: false,
-        nickname: null,
-      },
-      {
-        name: "Marcelo Trobbiani",
-        position: "midfielder",
-        number: 21,
-        isCaptain: false,
-        nickname: "Calesita",
-      },
-      {
-        name: "Héctor Zelada",
-        position: "goalkeeper",
-        number: 22,
-        isCaptain: false,
-        nickname: null,
-      },
-    ],
-  };
-
- 
+const select = document.getElementById("players");
+const players = myFavoriteFootballTeam.players;    
+const cardsList = document.getElementById("player-cards");
+Object.freeze(myFavoriteFootballTeam);
 
 //team stats
 team.textContent = myFavoriteFootballTeam.team;
 sport.textContent = myFavoriteFootballTeam.sport;
 year.textContent = myFavoriteFootballTeam.year;
-headCoach.textContent = myFavoriteFootballTeam.headCoach;
+coach.textContent = myFavoriteFootballTeam.headCoach.coachName;
 
+//filter cards
+const filterCards = ( arr = players) => {
+  cardsList.innerHTML = arr.map(
+    ({name, position, number, isCaptain, nickname}) => 
+      // don't use {} with arrow callback function
+    `<div class="player-card">
+      <h2>${isCaptain ? "(CAPTAIN)" : ""}${name}</h2>
+      <p>Position: ${position}</p>
+      <p>Number: ${number}</p>
+      <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+    </div>`
+  ).join("")
+};
 
 // event listener
-select.addEventListener ("change", (e) => {
-    console.log(e.target.value);
+select.addEventListener ("change", (e) => {        
+    console.log(e.target.value)  
+    cardsList.innerHTML = ""  
+    switch (e.target.value){
+      case "nickname":
+        filterCards(players.filter((item) => item.nickname !== null));        
+        break;      
+      case "forward":
+        filterCards(players.filter((item) => item.position === "forward"));        
+        break;      
+      case "midfielder":
+        filterCards(players.filter((item) => item.position === "midfielder"));        
+        break;      
+      case "defender":
+        filterCards(players.filter((item) => item.position === "defender"));        
+        break;
+      case "goalkeeper":
+        filterCards(players.filter((item) => item.position === "goalkeeper"));        
+         break;      
+      default:
+        filterCards();
+      }    
+    
 })
